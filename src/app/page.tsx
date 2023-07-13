d14857eca09d43340497932e39f4e509fb9cbf69
giorgio-villani <15069760+giorgio-villani@users.noreply.github.com>
2023-07-12 22:28:28 -0500

    added links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,21 @@ import Banner from "@/components/Banner";
 import Footer from "@/components/Footer";
 import Calendar from "@/components/Calendar";
 
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return (
-    <div className='app'>
-      <Banner />
-      <Calendar />
-      <Footer />
-    </div>
-  );
+  const [mount, setMount] = useState(false);
+  useEffect(() => {
+    setMount(true);
+  }, []);
+
+  if (mount) {
+    return (
+      <div className='app'>
+        <Banner />
+        <Calendar />
+        <Footer />
+      </div>
+    );
+  }
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,25 @@
 "use client";
 
 import styles from "@/styles/main.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div className={styles.footer}>Social Media</div>;
+  return (
+    <div className={styles.footer}>
+      <h4>Social Media</h4>
+      <span className={styles.icon}>
+        <Link href='https://www.instagram.com/hvtclxb/'>
+          <Image alt='insta' height={30} width={30} src='./instagram.svg' />
+        </Link>
+      </span>
+      <span className={styles.icon}>
+        <Link href='https://tiktok.com/@masondrums'>
+          <Image alt='tiktok' height={30} width={30} src='./tiktok.svg' />
+        </Link>
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
