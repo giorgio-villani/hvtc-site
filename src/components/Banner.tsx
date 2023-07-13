bace117156d5b7798e6b767c117fdea3f6f67731
giorgio-villani <15069760+giorgio-villani@users.noreply.github.com>
2023-07-13 12:13:29 -0500

    adjusted banner title

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,7 +6,7 @@ const Banner = () => {
   return (
     <div className={styles.banner}>
       <p>
-        <h1>Houston Vintage Truck Club</h1>
+        <h1 className={styles.logo}>Houston Vintage Truck Club</h1>
         <h3>------- Local Art & Tech -------</h3>
       </p>
     </div>
