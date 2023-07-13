18ec71d641669d98c6b7d4cce16ba4e98f90ff5f
giorgio-villani <15069760+giorgio-villani@users.noreply.github.com>
2023-07-12 22:36:11 -0500

    adjusted the calendar to agenda

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -6,7 +6,7 @@ const Calendar = () => {
   return (
     <div className={styles.calendar}>
       <iframe
-        src='https://calendar.google.com/calendar/embed?src=d752981dedde0e48195a368b76af0dd2e27874af7e2efd1c84365f9872594baa%40group.calendar.google.com&ctz=America%2FChicago'
+        src='https://calendar.google.com/calendar/embed?mode=AGENDA&src=d752981dedde0e48195a368b76af0dd2e27874af7e2efd1c84365f9872594baa%40group.calendar.google.com&ctz=America%2FChicago'
         style={{ border: 0, width: "100%" }}
         //   width='800'
         height='600'
